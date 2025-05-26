3e2e0308c4a170c7ae6ae4fc89ae6e31638dd218
Mustafa <smart1hashi@gmail.com>
2025-05-26 10:13:06 +0100

    % used example for old Dabase setup without using Di

--- app/src/main/java/com/hashinology/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/hashinology/borutoapp/data/local/BorutoDatabase.kt
@@ -1,10 +1,13 @@
 package com.hashinology.borutoapp.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.hashinology.borutoapp.data.local.dao.HeroDao
 import com.hashinology.borutoapp.data.local.dao.HeroRemoteKeyDao
+import com.hashinology.borutoapp.utils.Constants.BORUTO_DATABASE
 import com.hashinology.domain.model.Hero
 import com.hashinology.domain.model.HeroRemoteKey
 
@@ -16,4 +19,17 @@ import com.hashinology.domain.model.HeroRemoteKey
 abstract class BorutoDatabase: RoomDatabase() {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
+
+    /*// to use without using the di Old Style
+    companion object{
+        val instance: BorutoDatabase? = null
+        fun getInstance(context: Context): BorutoDatabase{
+            return Room.databaseBuilder(
+                context,
+                BorutoDatabase::class.java,
+                BORUTO_DATABASE
+            ).build()
+        }
+    }*/
+
 }
