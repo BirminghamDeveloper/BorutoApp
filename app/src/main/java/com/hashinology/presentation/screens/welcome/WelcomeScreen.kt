17b6fe3151f3b37da75a8aad16f8f0b3f60aec5d
Mustafa <smart1hashi@gmail.com>
2025-05-27 10:36:31 +0100

    + OnBoardingPage + Welcome Screen

---
+++ app/src/main/java/com/hashinology/presentation/screens/welcome/WelcomeScreen.kt
@@ -0,0 +1,198 @@
+package com.hashinology.presentation.screens.welcome
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.hashinology.borutoapp.R
+import com.hashinology.borutoapp.ui.theme.EXTRA_LARGE_PADDING
+import com.hashinology.borutoapp.ui.theme.SMALL_PADDING
+import com.hashinology.borutoapp.ui.theme.activeIndicatorColor
+import com.hashinology.borutoapp.ui.theme.descriptionColor
+import com.hashinology.borutoapp.ui.theme.titleColor
+import com.hashinology.borutoapp.ui.theme.welcomeScreenBackgroundColor
+import com.hashinology.borutoapp.utils.Constants.ON_BORADING_PAGE_COUNT
+import com.hashinology.domain.model.OnBoardingPage
+
+@Composable
+fun WelcomeScreen(navHostController: NavHostController) {
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        pageCount = { pages.size }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(welcomeScreenBackgroundColor)
+    ) {
+        HorizontalPager(
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+//        HorizontalPager(pagerState, activeIndicatorColor)
+    }
+}
+
+@Composable
+fun PagerScreen(onBoardingPage: OnBoardingPage) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = EXTRA_LARGE_PADDING),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Image(
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = stringResource(R.string.on_boarding_image),
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .weight(0.7f)                      // 70 % height for the image
+        )
+
+        Spacer(Modifier.height(SMALL_PADDING))
+
+        Text(
+            text = onBoardingPage.title,
+            color = titleColor,
+            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(SMALL_PADDING))
+
+        Text(
+            text = onBoardingPage.description,
+            color = descriptionColor,
+            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+/*
+@Composable
+fun WelcomeScreen(navHostController: NavHostController) {
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+    // new API: supply pageCount here
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        pageCount = { ON_BORADING_PAGE_COUNT }
+    )
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = welcomeScreenBackgroundColor)
+    ){
+        HorizontalPager(
+            state = pagerState,
+            verticalAlignment = Alignment.Top,
+
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+    }
+}
+
+@Composable
+fun PagerScreen(onBoardingPage: OnBoardingPage) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxSize(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = stringResource(R.string.on_boarding_image)
+        )
+    }
+    Text(modifier = Modifier
+        .fillMaxWidth(),
+        text = onBoardingPage.title,
+        color = titleColor,
+        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = EXTRA_LARGE_PADDING)
+            .padding(horizontal = SMALL_PADDING),
+        text = onBoardingPage.description,
+        color = descriptionColor,
+        fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+        fontWeight = FontWeight.Medium,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstOnBoardingScreenPreview() {
+    Column (modifier = Modifier.fillMaxSize()){
+        PagerScreen(onBoardingPage = OnBoardingPage.First)
+    }
+
+}
+
+}*/
+
+@Preview(showBackground = true)
+@Composable
+fun SecondOnBoardingScreenPreview() {
+    Column (modifier = Modifier.fillMaxSize()){
+        PagerScreen(onBoardingPage = OnBoardingPage.Second)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Third)
+    }
+}
