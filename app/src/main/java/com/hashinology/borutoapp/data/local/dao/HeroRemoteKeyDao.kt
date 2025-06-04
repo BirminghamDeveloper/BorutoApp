ac2dac3ca71eb2fd1f3595a9ce0cd29e3716294d
Mustafa <smart1hashi@gmail.com>
2025-06-04 12:20:08 +0100

    + RemoteKeys with Di

--- app/src/main/java/com/hashinology/borutoapp/data/local/dao/HeroRemoteKeyDao.kt
+++ app/src/main/java/com/hashinology/borutoapp/data/local/dao/HeroRemoteKeyDao.kt
@@ -4,14 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.hashinology.domain.model.HeroRemoteKey
+import com.hashinology.domain.model.HeroRemoteKeys
 
 @Dao
 interface HeroRemoteKeyDao {
     @Query("SELECT * FROM hero_remote_key_table WHERE id= :id")
-    suspend fun getRemoteKey(id: Int): HeroRemoteKey?
+    suspend fun getRemoteKeys(id: Int): HeroRemoteKeys?
     @Insert(onConflict= OnConflictStrategy.REPLACE)
-    suspend fun addAllRemoteKeys(heroRemoteKeys: List<HeroRemoteKey>)
+    suspend fun addAllRemoteKeys(heroRemoteKeys: List<HeroRemoteKeys>)
     @Query("Delete FROM HERO_REMOTE_KEY_TABLE")
     suspend fun deleteAllRemoteKeys()
 }
