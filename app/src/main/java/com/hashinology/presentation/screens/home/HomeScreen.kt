d3ca2d270e53d2af2e4bf40db92e23334fd36433
Mustafa <smart1hashi@gmail.com>
2025-06-09 06:57:49 +0100

    + HomeScreen

--- app/src/main/java/com/hashinology/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hashinology/presentation/screens/home/HomeScreen.kt
@@ -1,15 +1,32 @@
 package com.hashinology.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.hashinology.borutoapp.ui.theme.LARGE_PADDING
+import com.hashinology.presentation.components.RatingWidget
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeVM: HomeViewModel = hiltViewModel()
+) {
+    val allHeroes = homeVM.getAllHeroesUseCase.invoke().collectAsLazyPagingItems(
+
+    )
+
     Scaffold(
         topBar = {
             HomeTopBar( onSearchClicked = {} )
         }
-    ) {}
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 4.1
+        )
+    }
 }
