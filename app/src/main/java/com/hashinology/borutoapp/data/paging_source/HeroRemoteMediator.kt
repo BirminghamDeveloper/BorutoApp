c12d5e921425267c82e7a8cc4016435231685999
Mustafa <smart1hashi@gmail.com>
2025-06-25 11:04:48 +0100

    + icons % ShimmerEffect

--- app/src/main/java/com/hashinology/borutoapp/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/hashinology/borutoapp/data/paging_source/HeroRemoteMediator.kt
@@ -1,5 +1,6 @@
 package com.hashinology.borutoapp.data.paging_source
 
+import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
@@ -9,6 +10,9 @@ import com.hashinology.borutoapp.data.local.BorutoDatabase
 import com.hashinology.borutoapp.data.remote.BorutoApi
 import com.hashinology.domain.model.Hero
 import com.hashinology.domain.model.HeroRemoteKeys
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 @ExperimentalPagingApi
@@ -16,8 +20,28 @@ class HeroRemoteMediator @Inject constructor(
     private val borutoApi: BorutoApi,
     private val borutoDatabase: BorutoDatabase /*= BorutoDatabase.getInstance()*/
 ) : RemoteMediator<Int, Hero>() {
+
     private val heroDao = borutoDatabase.heroDao()
     private val heroRemoteKeysDao = borutoDatabase.heroRemoteKeysDao()
+
+    override suspend fun initialize(): InitializeAction {
+        val currentTime = System.currentTimeMillis()
+        val lastUpdated = heroRemoteKeysDao.getRemoteKeys(heroID = 1)?.lastUpdated ?: 0L
+        val cacheTimeout = 1440
+
+        Log.d("RemoteMediator", "Current Time: ${parseMillis(currentTime)}")
+        Log.d("RemoteMediator", "Last Updated Time: ${parseMillis(lastUpdated)}")
+
+        val diffInMinutes = (currentTime - lastUpdated) / 1000 / 60
+
+        return if (diffInMinutes.toInt() <= cacheTimeout) {
+            Log.d("RemoteMediator", "UP TO DATE!")
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            Log.d("RemoteMediator", "REFRESH")
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
 
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
         return try {
@@ -53,7 +77,8 @@ class HeroRemoteMediator @Inject constructor(
                         HeroRemoteKeys(
                             id = hero.id,
                             prevPage = prevPage,
-                            nextPage = nextPage
+                            nextPage = nextPage,
+                            lastUpdated = response.lastUpdated
                         )
                     }
                     heroRemoteKeysDao.addAllRemoteKeys(heroRemoteKeys = keys)
@@ -94,5 +119,11 @@ class HeroRemoteMediator @Inject constructor(
             ?.let { hero ->
                 heroRemoteKeysDao.getRemoteKeys(heroID = hero.id)
             }
+    }
+
+    private fun parseMillis(millis: Long): String{
+        val date = Date(millis)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.ROOT)
+        return format.format(date)
     }
 }
--- app/src/main/java/com/hashinology/presentation/components/ShimmerEffect.kt
+++ app/src/main/java/com/hashinology/presentation/components/ShimmerEffect.kt
@@ -1,24 +1,31 @@
 package com.hashinology.presentation.components
 
-import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.hashinology.borutoapp.ui.theme.ABOUT_PLACEHOLDER_HEIGHT
 import com.hashinology.borutoapp.ui.theme.EXTRA_SMALL_PADDING
 import com.hashinology.borutoapp.ui.theme.HERO_ITEM_HEIGHT
@@ -33,7 +40,31 @@ import com.hashinology.borutoapp.ui.theme.ShimmerMediumGray
 
 @Composable
 fun ShimmerEffect() {
+    LazyColumn(
+        contentPadding = PaddingValues(all = SMALL_PADDING),
+        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING)
+    ){
+        items(count = 2){
+            AnimatedShimmerItem()
+        }
+    }
+}
 
+@Composable
+fun AnimatedShimmerItem() {
+    val transition = rememberInfiniteTransition(label = "Infinite")
+    val alphaAnim by transition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 500,
+                easing = FastOutLinearInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
+    ShimmerItem(alpha = alphaAnim)
 }
 
 @Composable
@@ -93,11 +124,11 @@ fun ShimmerItem(alpha: Float) {
 @Preview
 @Composable
 private fun ShimmerItemPreview() {
-    ShimmerItem(0.5f)
+    AnimatedShimmerItem()
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 private fun ShimmerItemDarkPreview() {
-    ShimmerItem(0.5f)
+    AnimatedShimmerItem()
 }
